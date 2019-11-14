95726732736561c1c6e8b1c4fab4968a2248c76b
susruth <susruth@susruth.com>
2019-11-14 14:56:12 +1100

    contracts: updated import path

--- contracts/Example.sol
+++ contracts/Example.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.8;
 
-import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Example {
     using SafeMath for uint256;
